b558c802f97ac62af497c02d06947b7b23ce9a93
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-05 14:09:40 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -47,6 +47,20 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        json_data = request.get_json()
+        for key, value in json_data.items():
+            setattr(plant, key, value)
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response({}, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
